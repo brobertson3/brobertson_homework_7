c81e87b030cd08cee494a0644c29402449ee8000
Joni Halabi <her@thatdevgirl.com>
2016-05-04 23:32:35 -0400

    feedback on homework 7

--- solution_code/js/script.js
+++ solution_code/js/script.js
@@ -1,3 +1,13 @@
+/*
+	Hi Brent!  Great job on the homework!  The choice to use the lowercase version of the
+	city abbreviations is very clever.  Fantastic job with commenting your code too - it made
+	it immediately obvious what all of your code is doing.
+
+	Also... love the checkIdle function.  That definitely made me smile.
+
+	I have a couple of comments inline below, but they are pretty minor.
+*/
+
 $(document).ready(function () {
 	// Initialize the array with the city names
 	var cityArray = ["NYC", "SF", "LA", "ATX", "SYD"];
@@ -21,6 +31,11 @@ $(document).ready(function () {
 			/* If the user is idle then display prompt */
 			$('form').after('<p class="prompt">Please select a city in the drop-down menu to change the background.</p>')
 		} else {
+			/*
+				If you are doing nothing if the conditional in the if statement is false,
+				you do not need to include the else {} block.
+			*/
+
 			/* Do nothing because the user interacted with the dropdown menu */
 		}
 	}
@@ -31,6 +46,12 @@ $(document).ready(function () {
 		var selectedOption = $(this).val();
 		
 		// Change the class of the body to change the background of the page
+
+		/* 
+			The rubric asks to use an if/else if/ else statement to change the body class, but 
+			I like this solution much better - it is much more efficient.
+		*/
+		
 		$("body").attr("class", selectedOption);
 
 		/* If the prompt exists then at this point remove it */
